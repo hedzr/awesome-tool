6b430172eb2712619e5d1bd4da6f37ca77f10d61
Hedzr Yeh <hedzrz@gmail.com>
2020-10-07 11:20:31 +0800

    planning to new release

--- doc.go
+++ doc.go
@@ -8,9 +8,9 @@ const (
 	// AppName const
 	AppName = "awesome-tool" // main app-name
 	// Version const
-	Version = "1.0.7" // version name
+	Version = "1.1.0" // version name
 	// VersionInt const
-	VersionInt = 0x010007 // using as
+	VersionInt = 0x010100 // using as
 )
 
 // func GetApiPrefix() string {
