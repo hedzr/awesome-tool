19919d2bd7eb0074f4e8c015c38e4617d1128de9
hz <hedzrz@gmail.com>
2023-12-20 10:42:14 +0800

    bump to v1.2.1

--- doc.go
+++ doc.go
@@ -8,9 +8,9 @@ const (
 	// AppName const
 	AppName = "awesome-tool" // main app-name
 	// Version const
-	Version = "1.2.0" // version name
+	Version = "1.2.1" // version name
 	// VersionInt const
-	VersionInt = 0x010200 // using as
+	VersionInt = 0x010201 // using as
 )
 
 // func GetApiPrefix() string {
